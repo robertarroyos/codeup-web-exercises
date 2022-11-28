4f74cd7dbb67b98c694dab7c9d489b4a794eb34e
Robert Arroyos <robertarroyosjr@gmail.com>
2022-11-28 16:53:53 -0600

    Functions exercise

---
+++ js/conditional-lesson.js
@@ -0,0 +1,51 @@
+// let city = "San Antonio";
+// if (city == "Boston"){
+//     console.log("The city is Boston");
+//     console.log("Go Red Sox");
+// } else if (city == "San Antonio"){
+//     console.log("The city is San Antonio");
+//     console.log("Go Spurs")
+// }
+//
+// else{
+//     console.log("The condition is false.")
+// }
+//
+// console.log( addNumbers(5));
+// function addNumbers(num1){
+//     let number;
+//     if (typeof num1 == 'string'){
+//         return "This is not a number.";
+//         // number = parseInt(num1);
+//     }else{
+//         return num1 + 2;
+//     }
+//
+// }
+
+// var pizzaPreference = prompt("What kind of pizza do you like?");
+//
+// if (pizzaPreference === "pineapple and hot sauce") {
+//     alert("What a coincidence, that's my favorite!");
+// } else if (pizzaPreference === "cheese") {
+//     alert("Just plain cheese? Okay...");
+// } else {
+//     alert(pizzaPreference + " isn't my favorite, but let's order some!");
+// }
+// let success = true;
+// let message = (success) ? "The operation was successful." : "The operation was not successful."
+// console.log(message);
+
+// let pizzaPreference = prompt("What kind of pizza do you like?");
+// switch(pizzaPreference){
+//     case "cheese":
+//         alert("Cool. Me too.")
+//         break
+//     case "Pineapples and hot sauce":
+//         alert("Yikes!")
+//         break
+//     default:
+//         console.log('Oh, okay... thats cool... my aunt is vegan too')
+//         break;
+//
+// }
