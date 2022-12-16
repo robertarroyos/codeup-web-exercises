b4856365d19f0826e83ce78a2832d21fd347ced5
Robert Arroyos <robertarroyosjr@gmail.com>
2022-12-16 17:11:20 -0600

    Flexbox Exercises

--- js/javasript-exercise.js
+++ js/javasript-exercise.js
@@ -36,6 +36,46 @@
 //
 // console.log(findAverageDogAge(pets), 7.5);
 // ======================= returnPetWithNoFish Exercise ======================
+// const pets = [
+//     {
+//         name: 'Sparky',
+//         type: 'Fish',
+//         age: 4
+//     },
+//     {
+//         name: 'Mr. Pig',
+//         type: 'Cat',
+//         age: 4
+//     },
+//     {
+//         name: 'Bubba',
+//         type: 'Dog',
+//         age: 5
+//     },
+//     {
+//         name: 'Beans',
+//         type: 'Dog',
+//         age: 3
+//     },
+//     {
+//         name: 'Mr. Salmon',
+//         type: 'Fish',
+//         age: 1
+//     }
+// ];
+//
+// function returnPetWithNoFish(pets){
+//     let petsNewArr = [];
+//     pets.forEach((pet) => {
+//         if (pet.type !== 'Fish'){
+//             petsNewArr.push(pet);
+//         }
+//     });
+//     return petsNewArr;
+// }
+//
+// console.log(returnPetWithNoFish(pets))
+
 const pets = [
     {
         name: 'Sparky',
@@ -64,14 +104,3 @@ const pets = [
     }
 ];
 
-function returnPetWithNoFish(pets){
-    let petsNewArr = [];
-    pets.forEach((pet) => {
-        if (pet.type !== 'Fish'){
-            petsNewArr.push(pet);
-        }
-    });
-    return petsNewArr;
-}
-
-console.log(returnPetWithNoFish(pets))
