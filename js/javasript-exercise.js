5affcbc7e4914e7c6c57811686c12fda7e05c67b
Robert Arroyos <robertarroyosjr@gmail.com>
2023-01-18 12:20:42 -0600

    jquery FAQ exercise complete

--- js/javasript-exercise.js
+++ js/javasript-exercise.js
@@ -191,3 +191,88 @@
 //     console.log(i);
 // }
 
+// Write a function that takes a neighborhood object and determines if it is desirable. A neighborhood is desirable if the median home price is less than 300000, crime rates are low, and the total rating of schools is at least 24.
+
+// example data...
+
+const neighborhood1 = {
+    neighborhood: "Lovely Estates",
+    medianHomePrice: 280000,
+    pool: true,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 6},
+        {name: "High School", rating: 8}
+    ]
+};
+
+const neighborhood2 = {
+    neighborhood: "Luminous Estates",
+    medianHomePrice: 270000,
+    pool: true,
+    tennis: false,
+    crimeRate: "high",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 8},
+        {name: "High School", rating: 8}
+    ]
+}
+
+const neighborhood3 = {
+    neighborhood: "Oak Mountain",
+    medianHomePrice: 290000,
+    pool: false,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 8},
+        {name: "High School", rating: 8}
+    ]
+}
+
+const neighborhood4 = {
+    neighborhood: "Ginormous Acres",
+    medianHomePrice: 350000,
+    pool: true,
+    tennis: true,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 9},
+        {name: "Middle School", rating: 9},
+        {name: "High School", rating: 9}
+    ]
+}
+
+// fn(neighborhood1) // returns false due to school rating
+// fn(neighborhood2) // returns false due to crime rate
+// fn(neighborhood3) // returns true
+// fn(neighborhood4) // returns false due to median home price
+
+let neighborhoods = {neighborhood1, neighborhood2, neighborhood3, neighborhood4};
+
+function  goodNeighbor () {
+    let schoolRating = 0;
+    for (let i = 0; i < neighborhood4.schools.length; i++) {
+        schoolRating = schoolRating[i] + neighborhood4.schools.name.rating[i];
+        // return schoolRating;
+    }
+    return schoolRating;
+}
+
+
+// function goodNeighbor () {
+//     neighborhoods.forEach(function (neighborhood) {
+//         let schoolRatings = 0;
+//         neighborhoods.forEach(function (rating) {
+//             schoolRatings = schoolRatings + neighborhoods.schools.rating;
+//             return schoolRatings;
+//         });
+//         console.log(schoolRatings);
+        // if (neighborhood.medianHomePrice < 300000 && neighborhood.crimeRate === 'Low' &&) {
+        //     })
+// }
+// })}
